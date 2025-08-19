c91dcbe43346a69f603a7d72d7210dcbe6c3758d
PrithviChaudhary <prithabiterra@gmail.com>
2025-08-19 09:22:26 +0545

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <> 
         <img className='h-96 min-w-screen px-5 object-cover mx-auto' src="coverPhoto.jpeg" height={200} alt="cover" />
+        prithabi
     </>
   
   )
